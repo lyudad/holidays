63db60107d157542f43cd93ed2cceea9e827a1ca
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-11-04 17:04:23 +0000

    changed dashboard

--- src/components/AdminDashboard/index.tsx
+++ src/components/AdminDashboard/index.tsx
@@ -1,44 +1,63 @@
-// import { FC } from 'react';
+import React from 'react';
 import { Table } from 'antd';
-import { dashTable } from './styles';
-import { dataSource } from './data';
+import dashTable from './styles';
+// import { dataSource } from './data';
 
-const data = [
-  {
-    key: '2',
-    user: 'John',
-    dates: '05/11/21',
-    types: 'vacation',
-    actions: 'approve',
-  }];
+type Props = {};
 
-const columns = [
-  {
-    title: 'User',
-    dataIndex: 'user',
-    key: 'user',
-  },
-  {
-    title: 'Dates',
-    dataIndex: 'dates',
-    key: 'dates',
-  },
-  {
-    title: 'type',
-    dataIndex: 'vacation',
-    key: 'vacation',
-  },
-  {
-    title: 'Actions',
-    dataIndex: 'actions',
-    key: 'actions',
-  },
-];
+const Dash: React.FunctionComponent<Props> = () => {
+  const dataSource = [
+    {
+      key: '2',
+      user: 'John',
+      dates: '05/11/21',
+      types: 'vacation',
+      actions: 'approve',
+    },
+    {
+      key: '2',
+      user: 'John',
+      dates: '05/11/21',
+      types: 'vacation',
+      actions: 'approve',
+    },
+    {
+      key: '2',
+      user: 'John',
+      dates: '05/11/21',
+      types: 'vacation',
+      actions: 'approve',
+    }];
 
-const Dash = () => (
-  <div className="dashTable">
-    <Table columns={columns} dataSource={data} />
-  </div>
+  const columns = [
+    {
+      title: 'User',
+      dataIndex: 'user',
+      key: 'user',
+    },
+    {
+      title: 'Dates',
+      dataIndex: 'dates',
+      key: 'dates',
+    },
+    {
+      title: 'type',
+      dataIndex: 'vacation',
+      key: 'vacation',
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions',
+    },
+  ];
 
-);
+  return (
+    <div className={dashTable}>
+      <Table columns={columns} dataSource={dataSource} />
+    </div>
+
+  );
+};
+
 export default Dash;
--- src/components/AdminDashboard/styles.tsx
+++ src/components/AdminDashboard/styles.tsx
@@ -1,7 +1,12 @@
 import styled from 'styled-components';
 
-export const dashTable = styled.table`
+const dashTable = styled.div`
   width: 100%;
   height: 100%;
   max-width: 1200px;
+  color: grey;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
 `;
+export default dashTable;
