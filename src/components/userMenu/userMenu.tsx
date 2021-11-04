0016766ec1dcc5c4e88deca458f49bffc9c0d1cf
unknown <tanushaGK@i.ua>
2021-11-04 16:59:42 +0200

    fix

--- src/components/userMenu/userMenu.tsx
+++ src/components/userMenu/userMenu.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { Menu } from 'antd';
 import { StyledContainer, StyledSection } from 'components/userMenu/styles';
-import {
-  dashboard, profile, users, logout,
-} from 'components/userMenu/constants';
+// import {
+//   dashboard, profile, users, logout,
+// } from 'components/userMenu/constants';
+import LANG from 'language/en';
 import { IUser } from 'utils/types';
 
 // добавить обработчик на клик по кнопке, который будет переводить на соответствующие страницы ;
 // времено добавленный пользователь
-// const user: IUser  = {
+// const user: IUser = {
 //   _id: 'qwe',
 //   name: 'string',
 //   role: 'superAdmin',
@@ -16,25 +17,21 @@ import { IUser } from 'utils/types';
 
 // получаем роль у user  нам нужна именно роль, а не доступы
 
-const getUserRole = (user: IUser) => {
-  if (!user.role) {
-    return 'none';
-  }
-  return user.role;
-};
+const getUserRole = (user: IUser) => user.role;
 
 function UserMenu(user: IUser) {
   const MenuOptions = {
-    superAdmin: [dashboard, users, logout],
-    hrAdmin: [users, logout],
-    employee: [profile, logout],
+    superAdmin: [LANG.dashboard, LANG.users, LANG.logout],
+    hrAdmin: [LANG.users, LANG.logout],
+    employee: [LANG.profile, LANG.logout],
+    'No role': [],
   };
   const role = getUserRole(user);
 
   return (
     <StyledContainer>
       <Menu>
-        {MenuOptions[role].map((el) => (
+        { MenuOptions[role].map((el: string) => (
           <Menu.Item key={el}>
             {el}
           </Menu.Item>
