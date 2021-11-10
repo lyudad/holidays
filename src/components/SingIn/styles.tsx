342cce921880becf3e12c523a37d9e1bf9c9b9b8
unknown <tanushaGK@i.ua>
2021-11-10 22:06:40 +0200

    lint fix

--- src/components/SingIn/styles.tsx
+++ src/components/SingIn/styles.tsx
@@ -3,22 +3,22 @@ import styled from 'styled-components';
 import { Form, Input } from 'antd';
 
 export const StyledForm = styled(Form)`
- display:flex;
- flex-direction:column;
- margin-left:auto;
- margin-right:auto;
- margin-top:100px;
- width:400px;
+display:flex;
+flex-direction:column;
+margin-left:auto;
+margin-right:auto;
+margin-top:100px;
+width:400px;
 `;
 
 export const StyledInput = styled(Input)`
- margin-left:50px;
- margin-right:50px;
+margin-left:50px;
+margin-right:50px;
 margin-top:25px;
 
 `;
 export const StyledMessage = styled.div`
- margin-left:50px;
- margin-right: auto;
- margin-top: 10px;
+margin-left:50px;
+margin-right: auto;
+margin-top: 10px;
 `;
