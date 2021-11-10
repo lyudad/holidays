02583c2662a815f4e8e5e6b8f5b763260686283c
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-11-10 13:02:13 +0000

    admin dashboard

--- src/components/AdminDashboard/index.tsx
+++ src/components/AdminDashboard/index.tsx
@@ -1,62 +1,105 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-no-bind */
+/* eslint-disable @typescript-eslint/indent */
+/* eslint-disable linebreak-style */
 import React from 'react';
 import { Table } from 'antd';
-// import { dashTable } from './styles';
-// import { dataSource } from './data';
+import FormTable from './styles';
 
 type Props = {};
 
 const DashTable: React.FunctionComponent<Props> = () => {
-  const dataSource = [
-    {
-      key: '1',
-      user: 'John',
-      dates: '05/11/21',
-      types: 'vacation',
-      actions: 'approve',
-    },
-    {
-      key: '2',
-      user: 'Bill',
-      dates: '05/11/21',
-      types: 'vacation',
-      actions: 'approve',
-    },
-    {
-      key: '3',
-      user: 'Jack',
-      dates: '05/11/21',
-      types: 'vacation',
-      actions: 'approve',
-    }];
+  const data = [
+  {
+    key: '1',
+    user: 'User1',
+    dates: '05/11/21',
+    types: 'vacation',
+    actions: 'approve',
+  },
+  {
+    key: '2',
+    user: 'User2',
+    dates: '05/11/21',
+    types: 'vacation',
+    actions: 'approve',
+  },
+  {
+    key: '3',
+    user: 'User3',
+    dates: '05/11/21',
+    types: 'vacation',
+    actions: 'approve',
+  }];
 
-  const columns = [
-    {
-      title: 'User',
-      dataIndex: 'user',
-      key: 'user',
-    },
-    {
-      title: 'Dates',
-      dataIndex: 'dates',
-      key: 'dates',
-    },
-    {
-      title: 'type',
-      dataIndex: 'vacation',
-      key: 'vacation',
-    },
-    {
-      title: 'Actions',
-      dataIndex: 'actions',
-      key: 'actions',
-    },
-  ];
+const columns = [
+  {
+    title: 'User',
+    dataIndex: 'user',
+    key: 'user',
+    filters: [
+      {
+        text: 'User1',
+        value: 'User1',
+      },
+      {
+        text: 'User2',
+        value: 'User2',
+      },
+      {
+        text: 'User3',
+        value: 'User3',
+      },
+    ],
+  },
+  {
+    title: 'Dates',
+    dataIndex: 'dates',
+    key: 'dates',
+  },
+  {
+    title: 'Type',
+    dataIndex: 'type',
+    key: 'type',
+     filters: [
+      {
+        text: 'vacation',
+        value: 'vacation',
+      },
+      {
+        text: 'sick_leave',
+        value: 'sick_leave',
+      },
+    ],
+  },
+  {
+    title: 'Actions',
+    dataIndex: 'actions',
+    key: 'actions',
+     filters: [
+      {
+        text: 'approve',
+        value: 'approve',
+      },
+      {
+        text: 'decline',
+        value: 'decline',
+      },
+      {
+        text: 'edit',
+        value: 'edit',
+      },
+    ],
+  },
+];
 
+function onChange(filters: any) {
+  console.log('params', filters);
+}
   return (
-    <div className="dashTable">
-      <Table columns={columns} dataSource={dataSource} />
-    </div>
+    <FormTable>
+      <Table columns={columns} dataSource={data} onChange={onChange} />
+    </FormTable>
 
   );
 };
--- src/components/AdminDashboard/styles.tsx
+++ src/components/AdminDashboard/styles.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable linebreak-style */
 import styled from 'styled-components';
 
-const dashTable = styled.div`
-  width: 1200px;
+const FormTable = styled.div`
+  width: 100%;
   height: 100%;
+  max-width: 1200px;
   color: grey;
-  background: papayawhip;
   border: none;
   border-radius: 3px;
 `;
 
-export default dashTable;
+export default FormTable;
