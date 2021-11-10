dc1116f34f9a3380edff8ac4256eb9a34a9c0ac6
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-11-10 09:16:25 +0000

    type for function onChange(value)

--- src/components/Cascade/index.tsx
+++ src/components/Cascade/index.tsx
@@ -1,15 +1,18 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+/* eslint-disable max-len */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable quote-props */
 /* eslint-disable @typescript-eslint/naming-convention */
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable no-console */
 import { Cascader } from 'antd';
-import React, { FC, ChangeEvent } from 'react';
+import React, { FC } from 'react';
+import { CascaderValueType, CascaderOptionType } from 'antd/lib/cascader';
 import 'antd/dist/antd.css';
 import { DateCasc } from './styles';
 import { options } from './constants';
 
-function onChange(value: any) {
+function onChange(value: CascaderValueType, selectedOptions?: CascaderOptionType[] | undefined): any {
   console.log(value);
 }
 
@@ -20,3 +23,6 @@ const DateCascader:FC = () => (
 );
   
 export default DateCascader;
+function value(value: any) {
+  throw new Error('Function not implemented.');
+}
