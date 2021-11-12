d0cf2deb90383dd5767fbf40f703688ea80e164c
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-11-12 22:54:41 +0000

    added .eslintignore

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,10 @@
-import { NotFoundPage, HomePage, Calendar } from 'pages';
+import
+{
+  NotFoundPage,
+  HomePage,
+  Calendar,
+  AdminDash,
+} from 'pages';
 import ProfilePage from 'pages/userpage/ProfilePage';
 import UsersPage from 'pages/users';
 import LoginPage from 'pages/loginPage';
@@ -11,11 +17,11 @@ const App: FC = () => (
   <>
     <Switch>
       <Route path="/" exact component={HomePage} />
-      <Route path="/users" exact component={UsersPage} />
-      <Route path="/userpage" exact component={ProfilePage} />
+      <Route path="/users" component={UsersPage} />
+      <Route path="/userpage" component={ProfilePage} />
       <Route path="/users/calendar" component={Calendar} />
-      {/* <Route path="/users/dash" component={AdminDash} /> */}
-      {/* <Route path="login" exact render={() => <LoginPage />} /> */}
+      <Route path="/users/dash" component={AdminDash} />
+      <Route path="login" component={LoginPage} />
       {/* <ProtectedRoute path="/profile" exact children={<ProfileEditPage />} /> */}
       <Route path="*" exact component={NotFoundPage} />
     </Switch>
