25bff5683d31f69467678b645eb1a5bd8f98ce47
Aleksandra Knott <71516075+Sasha-create@users.noreply.github.com>
2021-11-14 20:46:46 +0000

    fexed eslint - added rules

--- src/components/AdminDashboard/index.tsx
+++ src/components/AdminDashboard/index.tsx
@@ -6,92 +6,92 @@ type Props = {};
 
 const DashTable: React.FunctionComponent<Props> = () => {
   const data = [
-  {
-    key: '1',
-    user: 'User1',
-    dates: '05/11/21',
-    types: 'vacation',
-    actions: 'approve',
-  },
-  {
-    key: '2',
-    user: 'User2',
-    dates: '05/11/21',
-    types: 'vacation',
-    actions: 'approve',
-  },
-  {
-    key: '3',
-    user: 'User3',
-    dates: '05/11/21',
-    types: 'vacation',
-    actions: 'approve',
-  }];
+    {
+      key: '1',
+      user: 'User1',
+      dates: '05/11/21',
+      types: 'vacation',
+      actions: 'approve',
+    },
+    {
+      key: '2',
+      user: 'User2',
+      dates: '05/11/21',
+      types: 'vacation',
+      actions: 'approve',
+    },
+    {
+      key: '3',
+      user: 'User3',
+      dates: '05/11/21',
+      types: 'vacation',
+      actions: 'approve',
+    }];
 
-const columns = [
-  {
-    title: 'User',
-    dataIndex: 'user',
-    key: 'user',
-    filters: [
-      {
-        text: 'User1',
-        value: 'User1',
-      },
-      {
-        text: 'User2',
-        value: 'User2',
-      },
-      {
-        text: 'User3',
-        value: 'User3',
-      },
-    ],
-  },
-  {
-    title: 'Dates',
-    dataIndex: 'dates',
-    key: 'dates',
-  },
-  {
-    title: 'Type',
-    dataIndex: 'type',
-    key: 'type',
-     filters: [
-      {
-        text: 'vacation',
-        value: 'vacation',
-      },
-      {
-        text: 'sick_leave',
-        value: 'sick_leave',
-      },
-    ],
-  },
-  {
-    title: 'Actions',
-    dataIndex: 'actions',
-    key: 'actions',
-     filters: [
-      {
-        text: 'approve',
-        value: 'approve',
-      },
-      {
-        text: 'decline',
-        value: 'decline',
-      },
-      {
-        text: 'edit',
-        value: 'edit',
-      },
-    ],
-  },
-];
+  const columns = [
+    {
+      title: 'User',
+      dataIndex: 'user',
+      key: 'user',
+      filters: [
+        {
+          text: 'User1',
+          value: 'User1',
+        },
+        {
+          text: 'User2',
+          value: 'User2',
+        },
+        {
+          text: 'User3',
+          value: 'User3',
+        },
+      ],
+    },
+    {
+      title: 'Dates',
+      dataIndex: 'dates',
+      key: 'dates',
+    },
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      key: 'type',
+      filters: [
+        {
+          text: 'vacation',
+          value: 'vacation',
+        },
+        {
+          text: 'sick_leave',
+          value: 'sick_leave',
+        },
+      ],
+    },
+    {
+      title: 'Actions',
+      dataIndex: 'actions',
+      key: 'actions',
+      filters: [
+        {
+          text: 'approve',
+          value: 'approve',
+        },
+        {
+          text: 'decline',
+          value: 'decline',
+        },
+        {
+          text: 'edit',
+          value: 'edit',
+        },
+      ],
+    },
+  ];
 
-function onChange(filters: any) {
-  console.log('params', filters);
-}
+  function onChange(filters: any) {
+    console.log('params', filters);
+  }
   return (
     <FormTable>
       <Table columns={columns} dataSource={data} onChange={onChange} />
