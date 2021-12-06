60ddbaedba9799f132aafbec03d311139c8b2861
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-12-06 18:38:18 +0200

    delete console.log

--- src/services/reducers/userPassword/userPassword-api-server.ts
+++ src/services/reducers/userPassword/userPassword-api-server.ts
@@ -1,7 +1,6 @@
 import axios from 'axios';
 import { UserValues } from './usePassword-types';
-// eslint-disable-next-line no-console
-console.log(process.env);
+
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 
 async function sendUserMail(data: UserValues) {
