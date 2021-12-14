36b00d2e0593bb3ade2ab59c5bd5b02acfc4081c
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-12-14 21:28:43 +0200

    added filter

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,5 @@
 import React, { FC, useEffect, useState } from 'react';
+import { Input } from 'antd';
 import { ColumnsType } from 'antd/es/table';
 import getUserList from 'services/api/userlistApi';
 import ActionButton from 'components/ActionButton';
@@ -25,7 +26,9 @@ interface User {
 
 const UsersPage: FC = () => {
   const [users, setUsers] = useState<User[]>([]);
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
   const [role, setRole] = useState<string>('');
+  const [filter, setFilter] = useState<string>('');
 
   const superAdminTableColumns: ColumnsType<any> = [{
     title: 'Name',
@@ -62,25 +65,51 @@ const UsersPage: FC = () => {
   useEffect(() => {
     const userRole = store.getState().user.userData.role;
     setRole(userRole);
-
     const token = {
       token: store.getState().user.token,
     };
-
     getUserList(token).then((data) => {
       if (data.length === 0) {
         return;
       }
       setUsers(data);
+      setFilteredUsers(data);
       // eslint-disable-next-line no-console
     }).catch((error) => console.log(error));
   }, []);
+
+  const onChange = (evt: any) => {
+    // eslint-disable-next-line no-console
+    // console.log(evt.currentTarget.value, 'evt');
+    const currentData = evt.currentTarget.value;
+    setFilter(currentData);
+    const findUsers = users.filter(
+      (user: any) => user.first_name.toLocaleLowerCase().includes(filter.toLowerCase()),
+    );
+    // return contacts.filter(contact =>
+    //   contact.name.toLocaleLowerCase().includes(filter.toLowerCase()),
+    // );
+    // first_name.toLocaleLowerCase().includes(filter.toLowerCase());
+    setFilteredUsers(findUsers);
+
+    // eslint-disable-next-line no-console
+    console.log(findUsers, 'evt');
+  };
 
   return (
     <StyledPage>
       <UserMenu />
 
       <StyledMain>
+        <Input
+          type="text"
+          value={filter}
+          name="filter"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
+          required
+          onChange={onChange}
+        />
         <ButtonWrap>
           <ActionButton
             block
@@ -99,7 +128,7 @@ const UsersPage: FC = () => {
                 <StyledTable
                   bordered
                   rowKey={(record: any): number => record.user_id}
-                  dataSource={users}
+                  dataSource={filteredUsers}
                   columns={superAdminTableColumns}
                   pagination={{
                     hideOnSinglePage: true,
--- src/utils/texts-constants.ts
+++ src/utils/texts-constants.ts
@@ -3,5 +3,5 @@ export const USERS_TABLE_COL_USERS_NAME: string = 'USERS';
 export const USERS_TABLE_COL_ACTIONS_NAME: string = 'ACTIONS';
 export const EMPLOYEE_ROLE: string = 'employee';
 export const SUPER_ADMIN_ROLE: string = 'super_admin';
-export const ADMIN_ROLE:string = 'super_admin';
+export const ADMIN_ROLE:string = 'admin';
 export const LOGIN_ERROR:string = 'Something went wrong... check your username and password';
