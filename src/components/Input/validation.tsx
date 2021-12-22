0ba33e5dfa07653727bdd54da4b5e98186f83296
Shevchuk-Ren <shevchuk_re@ukr.net>
2021-12-22 11:38:33 +0200

    added validation

--- src/components/Input/validation.tsx
+++ src/components/Input/validation.tsx
@@ -1,8 +1,16 @@
 import * as yup from 'yup';
 
 const validation = {
-  firstName: yup.string().matches(/^([^0-9]*)$/, 'First Name should not contain numbers').min(2, 'First Name must be longer').max(10, 'First Name must be shorter'),
-  lastName: yup.string().matches(/^([^0-9]*)$/, 'Last Name should not contain numbers').min(2, 'Last Name must be longer').max(10, 'Last name must be shorter'),
+  firstName: yup.string()
+    .matches(/^([^0-9]*)$/, 'First Name should not contain numbers')
+    .matches(/[^\s]/, 'First Name should not contain gap')
+    .min(2, 'First Name must be longer')
+    .max(10, 'First Name must be shorter'),
+  lastName: yup.string()
+    .matches(/^([^0-9]*)$/, 'Last Name should not contain numbers')
+    .matches(/[^\s]/, 'Last Name should not contain gap')
+    .min(2, 'Last Name must be longer')
+    .max(10, 'Last name must be shorter'),
   email: yup.string().email().min(4, 'Email must be longer').max(30, 'Email must be shorter'),
 };
 const schema = yup.object(validation).required();
